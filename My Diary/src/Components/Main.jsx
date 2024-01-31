1e8dc95720a57fe978fd60d216024a87d5243cf2
KevinChiputra <kevin.chiputra@binus.ac.id>
2024-01-31 15:15:12 +0700

    Tambahan Google Script

--- My Diary/src/Components/Main.jsx
+++ My Diary/src/Components/Main.jsx
@@ -1,6 +1,23 @@
 import React from 'react'
 
 export const Main = () => {
+
+    const submit = (e) =>{
+        const formEle = document.querySelector('form')
+        e.preventDefault()
+
+        
+        console.log("berhasil")
+        const formData = new FormData(formEle)
+        fetch("https://script.google.com/macros/s/AKfycbxjX6Ch_uJkdfDNVs6isoceBmKM_5qlAdrief-Cis4ga0bsqVWpa3Z8A8pOQyIwcp5r/exec", {
+            method: "POST",
+            body: formData
+        }).then((response) => response.json()).then((result) => {
+            console.log(result)
+        })
+        
+    }
+
   return (
     <div className='bg-[#7B66FF] w-screen h-screen flex flex-col items-center justify-center'>
         
@@ -9,11 +26,12 @@ export const Main = () => {
             <h2 className='text-2xl text-[#ffffff] font-poppins font-light'>Type about your story today</h2>
         </div>
 
-        <div className='flex flex-col gap-4 w-[50%]'>
-            <input type="date" className='p-4 rounded-md border-4 border-black text-xl'/>
-            <input type="text" placeholder='Mood' className='p-4 rounded-md border-4 border-black text-xl'/>
-            <textarea name="" id="" cols="30" rows="10" placeholder='Apa yang terjadi hari ini?' className='p-4 rounded-md border-4 border-black text-xl resize-none'></textarea>
-        </div>
+        <form className='flex flex-col gap-4 w-[50%] items-center' onSubmit={submit} id='form'>
+            <input type="date" className='p-4 rounded-md border-4 border-black text-xl w-full' name='Tanggal'/>
+            <input type="text" placeholder='Mood' className='p-4 rounded-md border-4 border-black text-xl w-full' name='Mood'/>
+            <textarea name="Message" id="" cols="30" rows="10" placeholder='Apa yang terjadi hari ini?' className='p-4 rounded-md border-4 border-black text-xl resize-none w-full'></textarea>
+            <input className='w-[30%] h-10 bg-[#5FBDFF] font-poppins rounded-md font-medium text-white' type='submit'/>
+        </form>
     </div>
   )
 }
